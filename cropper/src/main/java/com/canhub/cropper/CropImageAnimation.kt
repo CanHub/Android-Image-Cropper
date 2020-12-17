b035def4fb863ef3a485f7608aa049458d2d75f4
Canato <vcanato@gmail.com>
2020-12-17 18:49:05 +0000

    update CropImageAnimation to kotlin

--- cropper/src/main/java/com/canhub/cropper/CropImageAnimation.kt
+++ cropper/src/main/java/com/canhub/cropper/CropImageAnimation.kt
@@ -1,121 +1,104 @@
-// "Therefore those skilled at the unorthodox
-// are infinite as heaven and earth,
-// inexhaustible as the great rivers.
-// When they come to an end,
-// they begin again,
-// like the days and months;
-// they die and are reborn,
-// like the four seasons."
-//
-// - Sun Tsu,
-// "The Art of War"
+package com.canhub.cropper
 
-package com.canhub.cropper;
-
-import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.Transformation;
-import android.widget.ImageView;
+import android.graphics.Matrix
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.graphics.RectF
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Transformation
+import android.widget.ImageView
 
 /**
  * Animation to handle smooth cropping image matrix transformation change, specifically for
  * zoom-in/out.
  */
-final class CropImageAnimation extends Animation implements Animation.AnimationListener {
+internal class CropImageAnimation(
+    private val imageView: ImageView, private val cropOverlayView: CropOverlayView
+) : Animation(), AnimationListener {
 
-  // region: Fields and Consts
+    private val startBoundPoints = FloatArray(8)
+    private val endBoundPoints = FloatArray(8)
+    private val startCropWindowRect = RectF()
+    private val endCropWindowRect = RectF()
+    private val startImageMatrix = FloatArray(9)
+    private val endImageMatrix = FloatArray(9)
 
-  private final ImageView mImageView;
-
-  private final CropOverlayView mCropOverlayView;
-
-  private final float[] mStartBoundPoints = new float[8];
-
-  private final float[] mEndBoundPoints = new float[8];
-
-  private final RectF mStartCropWindowRect = new RectF();
-
-  private final RectF mEndCropWindowRect = new RectF();
-
-  private final float[] mStartImageMatrix = new float[9];
-
-  private final float[] mEndImageMatrix = new float[9];
-
-  private final RectF mAnimRect = new RectF();
-
-  private final float[] mAnimPoints = new float[8];
-
-  private final float[] mAnimMatrix = new float[9];
-  // endregion
-
-  public CropImageAnimation(ImageView cropImageView, CropOverlayView cropOverlayView) {
-    mImageView = cropImageView;
-    mCropOverlayView = cropOverlayView;
-
-    setDuration(300);
-    setFillAfter(true);
-    setInterpolator(new AccelerateDecelerateInterpolator());
-    setAnimationListener(this);
-  }
-
-  public void setStartState(float[] boundPoints, Matrix imageMatrix) {
-    reset();
-    System.arraycopy(boundPoints, 0, mStartBoundPoints, 0, 8);
-    mStartCropWindowRect.set(mCropOverlayView.getCropWindowRect());
-    imageMatrix.getValues(mStartImageMatrix);
-  }
-
-  public void setEndState(float[] boundPoints, Matrix imageMatrix) {
-    System.arraycopy(boundPoints, 0, mEndBoundPoints, 0, 8);
-    mEndCropWindowRect.set(mCropOverlayView.getCropWindowRect());
-    imageMatrix.getValues(mEndImageMatrix);
-  }
-
-  @Override
-  protected void applyTransformation(float interpolatedTime, Transformation t) {
-
-    mAnimRect.left =
-        mStartCropWindowRect.left
-            + (mEndCropWindowRect.left - mStartCropWindowRect.left) * interpolatedTime;
-    mAnimRect.top =
-        mStartCropWindowRect.top
-            + (mEndCropWindowRect.top - mStartCropWindowRect.top) * interpolatedTime;
-    mAnimRect.right =
-        mStartCropWindowRect.right
-            + (mEndCropWindowRect.right - mStartCropWindowRect.right) * interpolatedTime;
-    mAnimRect.bottom =
-        mStartCropWindowRect.bottom
-            + (mEndCropWindowRect.bottom - mStartCropWindowRect.bottom) * interpolatedTime;
-    mCropOverlayView.setCropWindowRect(mAnimRect);
-
-    for (int i = 0; i < mAnimPoints.length; i++) {
-      mAnimPoints[i] =
-          mStartBoundPoints[i] + (mEndBoundPoints[i] - mStartBoundPoints[i]) * interpolatedTime;
+    init {
+        duration = 300
+        fillAfter = true
+        interpolator = AccelerateDecelerateInterpolator()
+        setAnimationListener(this)
     }
-    mCropOverlayView.setBounds(mAnimPoints, mImageView.getWidth(), mImageView.getHeight());
 
-    for (int i = 0; i < mAnimMatrix.length; i++) {
-      mAnimMatrix[i] =
-          mStartImageMatrix[i] + (mEndImageMatrix[i] - mStartImageMatrix[i]) * interpolatedTime;
+    fun setStartState(boundPoints: FloatArray?, imageMatrix: Matrix) {
+        reset()
+        System.arraycopy(boundPoints, 0, startBoundPoints, 0, 8)
+        startCropWindowRect.set(cropOverlayView.cropWindowRect)
+        imageMatrix.getValues(startImageMatrix)
     }
-    Matrix m = mImageView.getImageMatrix();
-    m.setValues(mAnimMatrix);
-    mImageView.setImageMatrix(m);
 
-    mImageView.invalidate();
-    mCropOverlayView.invalidate();
-  }
+    fun setEndState(boundPoints: FloatArray?, imageMatrix: Matrix) {
+        System.arraycopy(boundPoints, 0, endBoundPoints, 0, 8)
+        endCropWindowRect.set(cropOverlayView.cropWindowRect)
+        imageMatrix.getValues(endImageMatrix)
+    }
 
-  @Override
-  public void onAnimationStart(Animation animation) {}
+    override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
 
-  @Override
-  public void onAnimationEnd(Animation animation) {
-    mImageView.clearAnimation();
-  }
+        val animRect = RectF()
+        animRect.left = (
+            startCropWindowRect.left
+                + (endCropWindowRect.left - startCropWindowRect.left)
+                * interpolatedTime
+            )
+        animRect.top = (
+            startCropWindowRect.top
+                + (endCropWindowRect.top - startCropWindowRect.top)
+                * interpolatedTime
+            )
+        animRect.right = (
+            startCropWindowRect.right
+                + (endCropWindowRect.right - startCropWindowRect.right)
+                * interpolatedTime
+            )
+        animRect.bottom = (
+            startCropWindowRect.bottom
+                + (endCropWindowRect.bottom - startCropWindowRect.bottom)
+                * interpolatedTime
+            )
 
-  @Override
-  public void onAnimationRepeat(Animation animation) {}
+        cropOverlayView.cropWindowRect = animRect
+
+        val animPoints = FloatArray(8)
+        for (i in animPoints.indices) {
+            animPoints[i] = (
+                startBoundPoints[i]
+                    + (endBoundPoints[i] - startBoundPoints[i])
+                    * interpolatedTime
+                )
+        }
+        cropOverlayView.setBounds(animPoints, imageView.width, imageView.height)
+
+        val animMatrix = FloatArray(9)
+        for (i in animMatrix.indices) {
+            animMatrix[i] = (
+                startImageMatrix[i]
+                    + (endImageMatrix[i] - startImageMatrix[i])
+                    * interpolatedTime
+                )
+        }
+        val matrix = imageView.imageMatrix
+
+        matrix.setValues(animMatrix)
+        imageView.imageMatrix = matrix
+        imageView.invalidate()
+        cropOverlayView.invalidate()
+    }
+
+    override fun onAnimationStart(animation: Animation) {}
+    override fun onAnimationEnd(animation: Animation) {
+        imageView.clearAnimation()
+    }
+
+    override fun onAnimationRepeat(animation: Animation) {}
 }
