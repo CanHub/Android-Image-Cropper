5c3ebcd6d8038765184b067611f40fcb00d4e3f0
Victor Canato <vcanato@gmail.com>
2021-02-23 11:37:03 +0000

    Sample/crop image (#63)
    * Add camera module
    
    * Update appcompat version to use new permissions
    
    * add file provider for camera storage in sample

---
+++ sample/src/main/java/com/canhub/cropper/sample/camera/domain/CameraContract.kt
@@ -0,0 +1,31 @@
+package com.canhub.cropper.sample.camera.domain
+
+import android.content.Context
+import android.content.Intent
+import androidx.fragment.app.FragmentActivity
+
+internal interface CameraContract {
+
+    interface View {
+        fun startCropImage(option: CameraEnumDomain)
+        fun showErrorMessage(message: String)
+        fun dispatchTakePictureIntent()
+        fun cameraPermissionLaunch()
+        fun showDialog()
+        fun handleCropImageResult(uri: String)
+    }
+
+    interface Presenter {
+        fun bind(view: View)
+        fun unbind()
+        fun onPermissionResult(granted: Boolean)
+        fun onCreate(activity: FragmentActivity?, context: Context?)
+        fun onOk()
+        fun onCancel()
+        fun onActivityResult(resultCode: Int, requestCode: Int, data: Intent?)
+        fun startWithUriClicked()
+        fun startWithoutUriClicked()
+        fun startPickImageActivityClicked()
+        fun startActivityForResultClicked()
+    }
+}
