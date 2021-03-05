32945019927021e48ed38e9faeaa68f67820f269
Canato <vcanato@gmail.com>
2021-03-05 18:54:59 +0000

    remove class comments

--- cropper/src/main/java/com/canhub/cropper/CropWindowHandler.kt
+++ cropper/src/main/java/com/canhub/cropper/CropWindowHandler.kt
@@ -1,15 +1,3 @@
-// "Therefore those skilled at the unorthodox
-// are infinite as heaven and earth,
-// inexhaustible as the great rivers.
-// When they come to an end,
-// they begin again,
-// like the days and months;
-// they die and are reborn,
-// like the four seasons."
-//
-// - Sun Tsu,
-// "The Art of War"
-
 package com.canhub.cropper
 
 import android.graphics.RectF
