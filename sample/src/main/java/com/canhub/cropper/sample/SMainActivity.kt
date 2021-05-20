0aab45aae6f8a93a360e4cd1d6ef88780fa24a40
Ashique Bava <ashiquebava.ab@gmail.com>
2021-05-20 10:41:14 +0530

    Bug fixes in sample applicatioin and library

--- sample/src/main/java/com/canhub/cropper/sample/SMainActivity.kt
+++ sample/src/main/java/com/canhub/cropper/sample/SMainActivity.kt
@@ -2,7 +2,9 @@ package com.canhub.cropper.sample
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import com.canhub.cropper.sample.camera.app.SCameraFragment
 import com.canhub.cropper.sample.camera_java.app.SCameraFragmentJava
 import com.canhub.cropper.sample.crop_image_view.app.SCropImageViewFragment
@@ -12,9 +14,11 @@ import com.example.croppersample.databinding.ActivityMainBinding
 
 internal class SMainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
@@ -22,6 +26,7 @@ internal class SMainActivity : AppCompatActivity() {
             hideButtons(binding)
             supportFragmentManager
                 .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .replace(R.id.container, SCropImageViewFragment.newInstance())
                 .commit()
         }
@@ -34,6 +39,7 @@ internal class SMainActivity : AppCompatActivity() {
             hideButtons(binding)
             supportFragmentManager
                 .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .replace(R.id.container, SCameraFragment.newInstance())
                 .commit()
         }
@@ -42,13 +48,26 @@ internal class SMainActivity : AppCompatActivity() {
             hideButtons(binding)
             supportFragmentManager
                 .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .replace(R.id.container, SCameraFragmentJava.newInstance())
                 .commit()
+        }
+
+        onBackPressedDispatcher.addCallback {
+            if (binding.container.visibility == View.VISIBLE) {
+                binding.image.visibility = View.VISIBLE
+                binding.buttons.visibility = View.VISIBLE
+                binding.container.visibility = View.GONE
+                binding.container.removeAllViews()
+            } else {
+                finish()
+            }
         }
     }
 
     private fun hideButtons(binding: ActivityMainBinding) {
         binding.image.visibility = View.GONE
         binding.buttons.visibility = View.GONE
+        binding.container.visibility = View.VISIBLE
     }
 }
