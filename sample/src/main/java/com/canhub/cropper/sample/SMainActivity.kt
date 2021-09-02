338bddbc2575544d55565d31f8154628d1c0fdb8
James471 <56861963+James471@users.noreply.github.com>
2021-09-03 00:07:57 +0530

    Reproduce Bug

--- sample/src/main/java/com/canhub/cropper/sample/SMainActivity.kt
+++ sample/src/main/java/com/canhub/cropper/sample/SMainActivity.kt
@@ -1,8 +1,17 @@
 package com.canhub.cropper.sample
 
+import android.content.ContentResolver
+import android.content.res.Resources
+import android.graphics.Rect
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageView
+import com.canhub.cropper.options
 import com.canhub.cropper.sample.crop_image.app.SCropImageFragment
 import com.canhub.cropper.sample.crop_image_java.app.SCropImageFragmentJava
 import com.canhub.cropper.sample.crop_image_view.app.SCropImageViewFragment
@@ -10,6 +19,16 @@ import com.canhub.cropper.sample.extend_activity.app.SExtendActivity
 import com.example.croppersample.databinding.ActivityMainBinding
 
 internal class SMainActivity : AppCompatActivity() {
+
+    private val croppingRectangle: Rect = Rect(1239, 544, 2386, 1036)
+
+    private val cropImage: ActivityResultLauncher<CropImageContractOptions> =
+        registerForActivityResult(
+            CropImageContract()
+        ) { result: CropImageView.CropResult ->
+//            Place a breakpoint in the next line
+            val rect: Rect? = result.cropRect //The rectangle cropped out. If things go right, it should be equal to Rect(1239, 544, 2386, 1036).
+        }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -34,6 +53,31 @@ internal class SMainActivity : AppCompatActivity() {
         binding.sampleCropImageJava.setOnClickListener {
             SCropImageFragmentJava.newInstance().show()
         }
+
+        binding.reproduceBug.setOnClickListener {
+            val resources: Resources =
+                applicationContext.resources
+            val resourceId = resources.getIdentifier("reproduce_bug", "drawable", packageName)
+            val uri: Uri = Uri.Builder()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .authority(resources.getResourcePackageName(resourceId))
+                .appendPath(resources.getResourceTypeName(resourceId))
+                .appendPath(resources.getResourceEntryName(resourceId))
+                .build()
+            val height = croppingRectangle.bottom-croppingRectangle.top
+            val width = croppingRectangle.right-croppingRectangle.left
+            cropImage.launch(
+                options(uri) {
+                    setInitialCropWindowRectangle(croppingRectangle)
+                    setRequestedSize(width, height)
+                    setMinCropResultSize(width, height)
+                    setMaxCropResultSize(width, height)
+                    setMinCropWindowSize(width, height)
+                    setFixAspectRatio(false)
+                }
+            )
+        }
+
     }
 
     private fun Fragment.show() {
