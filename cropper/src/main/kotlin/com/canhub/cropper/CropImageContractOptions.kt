062a8b1c5e987a079dacfc5cb7befc56a75befdb
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-08-05 11:55:21 +0200

    API: Deprecate CropImageContract.

--- cropper/src/main/kotlin/com/canhub/cropper/CropImageContractOptions.kt
+++ cropper/src/main/kotlin/com/canhub/cropper/CropImageContractOptions.kt
@@ -2,6 +2,12 @@ package com.canhub.cropper
 
 import android.net.Uri
 
+@Deprecated(
+  """
+  This ActivityResultContract is deprecated.
+  Please either roll your own ActivityResultContract with the desired behavior or copy paste this.
+""",
+)
 data class CropImageContractOptions(
   val uri: Uri?,
   val cropImageOptions: CropImageOptions,
